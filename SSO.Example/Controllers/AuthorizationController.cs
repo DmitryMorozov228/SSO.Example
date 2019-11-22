ac38968569e491c93344178783fc8538aa1464da
Dmitriy Morozov <dmitriy_morozov_1997@mail.ru>
2019-11-22 15:41:52 +0300

    Initial project.

---
+++ SSO.Example/Controllers/AuthorizationController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSO.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private const string PublicMessageData = "This is public data";
+        private const string PrivateMessageData = "This is private data";
+
+        [HttpGet]
+        [Route("public")]
+        public ActionResult<string> GetPublicData()
+        {
+            return PublicMessageData;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<string> LoginAsync()
+        {
+            await HttpContext.ChallengeAsync("Auth0");
+            return await HttpContext.GetTokenAsync("access_token");
+        }
+        
+        [HttpGet]
+        [Authorize(Policy = "AuthorizedUser")]
+        [Route("private")]
+        public ActionResult<string> GetPrivateData()
+        {
+            return PrivateMessageData;
+        }
+    }
+}
---
+++ SSO.Example/Startup.cs
@@ -0,0 +1,104 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SSO.Example
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            #region Authentication
+
+            // Add authentication services
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = Configuration["Auth0:AuthorityUrl"];
+                    options.RequireHttpsMetadata = false;
+                    options.Audience = "https://dev-tfw0kkbs.auth0.com/api/v2/";
+                })
+                .AddOpenIdConnect("Auth0", options =>
+                {
+                    options.SaveTokens = true;
+                    // Set the authority to your Auth0 domain
+                    options.Authority = Configuration["Auth0:AuthorityUrl"];
+
+                    // Configure the Auth0 Client ID and Client Secret
+                    options.ClientId = Configuration["Auth0:ClientId"];
+                    options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+                    // Configure the scope
+                    options.Scope.Clear();
+                    options.Scope.Add("openid");
+
+                    // Set the callback path, so Auth0 will call back to http://localhost:6102/callback
+                    // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+                    options.CallbackPath = new PathString("/callback");
+                    options.ClaimsIssuer = "Auth0";
+                    options.Events = new OpenIdConnectEvents
+                    {
+                        OnRedirectToIdentityProvider = context =>
+                        {
+                            context.ProtocolMessage.SetParameter("audience", "https://dev-tfw0kkbs.auth0.com/api/v2/");
+                            return Task.FromResult(0);
+                        },
+                    };
+                });
+
+
+            #endregion
+
+            #region Authorization
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AuthorizedUser", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .Build());
+            });
+            
+            #endregion
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
